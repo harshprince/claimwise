bf63f22a47f8ce35c91c7b0104508d93ed08afe9
harshsinhaprince <136982625+harshsinhaprince@users.noreply.github.com>
2025-04-25 00:10:10 +0530

    sprint2 jira changes
    sprint2 jira changes

--- gateway/auth-service/src/main/java/com/authservice/controller/Controller.java
+++ gateway/auth-service/src/main/java/com/authservice/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.authservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,9 @@ public class Controller {
 	  @GetMapping("/auth/status")
 	    public String status() {
 	        return "Hi Auth Service is UP and working!";
+	    }
+	  @PostMapping("/auth/login")
+	    public ResponseEntity<String> login() {
+	        return ResponseEntity.ok("Login successful");
 	    }
 }
